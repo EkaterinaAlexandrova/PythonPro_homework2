cf4bcc7409374307b742183d3da6a2989c929a8e
EkaterinaAlexandrova <ekaterinaaalexandrovaa@gmail.com>
2023-06-17 17:29:24 +0200

    Homework version 2.0

--- drunk_polish_calculator_test.py
+++ drunk_polish_calculator_test.py
@@ -1,61 +1,41 @@
 from drunk_polish_calculator import op_plus, op_minus, op_divide, op_multiply, main
-from io import StringIO
-import sys
+from unittest.mock import patch
 import pytest
 
-def test_op_plus():
-    assert op_plus(2, 3) == 5
-    assert op_plus(-1, 5) == 4
-    assert op_plus(0, 0) == 0
+@pytest.mark.parametrize("x,y,expected_result", [(2,3,5), (-1,5,4), (0,0,0)])
+def test_op_plus(x:float, y:float, expected_result:float):
+    result = op_plus(x, y)
+    assert result == expected_result
 
+@pytest.mark.parametrize("x,y,expected_result", [(5,2,3), (10,10,0), (5,-3,8)])
+def test_op_minus(x:float, y:float, expected_result:float):
+    result = op_minus(y, x)
+    assert result == expected_result
 
-def test_op_minus():
-    assert op_minus(5, 2) == 3
-    assert op_minus(10, 10) == 0
-    assert op_minus(3, -5) == -2
+@pytest.mark.parametrize("x,y,expected_result", [(2,3,6), (0,10,0), (-4,5,-20)])
+def test_op_multiply(x:float, y:float, expected_result:float):
+    result = op_multiply(x, y)
+    assert result == expected_result
 
-
-def test_op_divide():
-    assert op_multiply(2, 3) == 6
-    assert op_multiply(0, 10) == 0
-    assert op_multiply(-4, 5) == -20
+@pytest.mark.parametrize("x,y,expected_result", [(10, 2, 5), (7, 2, 3.5), (0, 5, 0)])
+def test_op_divide(x: float, y: float, expected_result: float):
+    result = op_divide(x, y)
+    assert result == expected_result
     with pytest.raises(ZeroDivisionError):
         op_divide(1, 0)
 
 
-def test_op_multiplu():
-    assert op_divide(10, 2) == 5
-    assert op_divide(7, 2) == 3.5
-    assert op_divide(0, 5) == 0
+@pytest.mark.parametrize(
+    "expression, expected_result",
+    [
+        ("1 1 +", "2.0"),
+        ("5 2 -", "3.0"),
+        ("5 4 *", "20.0"),
+        ("8 2 /", "4.0")
+    ]
+)
 
-def test_main_op_plus():
-    sys.stdin = StringIO('8 3 +')
-    sys.stdout = result = StringIO()
-    main()
-    assert result.getvalue().strip() == 'Expression with space delimiter:11.0'
-
-def test_main_op_minus():
-    sys.stdin = StringIO('5 2 -')
-    sys.stdout = result = StringIO()
-    main()
-    assert result.getvalue().strip() == 'Expression with space delimiter:3.0'
-
-def test_main_op_multiply():
-    sys.stdin = StringIO('2 5 *')
-    sys.stdout = result = StringIO()
-    main()
-    assert result.getvalue().strip() == 'Expression with space delimiter:10.0'
-
-def test_main_op_divide():
-    sys.stdin = StringIO('6 2 /')
-    sys.stdout = result = StringIO()
-    main()
-    assert result.getvalue().strip() == 'Expression with space delimiter:3.0'
-
-def test_main_output(capsys):
-    input_string = "2 3 + 4 *"
-    expected_output = "20.0\n"
-    with pytest.raises(StopIteration):
-        main(input_string)
-        captured = capsys.readouterr()
-        assert captured.out == expected_output
+def test_main_output(capsys, expression, expected_result):
+    with patch("builtins.input", return_value=expression):
+        main()
+    assert capsys.readouterr().out.rstrip() == expected_result
